9d733538e9bba588c1f85f6753b594a735e12902
mhossain2 <moinulhossainmahim@gmail.com>
2024-06-27 02:14:08 +0600

    updated hero gallery images

--- components/shared/hero/HeroImageGallery.tsx
+++ components/shared/hero/HeroImageGallery.tsx
@@ -6,9 +6,15 @@ const HeroImageGallery = () => {
             <img src="/assets/hero-1.png" loading="lazy" alt="Photo by Minh Pham" className="absolute inset-0 h-full w-full object-cover object-center" />
         </div>
 
-        <div className="group relative flex h-48 items-end overflow-hidden rounded-lg bg-gray-100 shadow-lg md:h-full md:row-span-2">
-            <img src="/assets/hero-4.png" loading="lazy" alt="Photo by Magicle" className="absolute inset-0 h-full w-full object-cover object-center" />
-            <div className="pointer-events-none absolute inset-0 bg-gradient-to-t from-gray-800 via-transparent to-transparent opacity-50"></div>
+        <div className="group relative flex h-48 items-end overflow-hidden rounded-lg bg-gray-100 shadow-lg md:h-full md:row-span-2 bg-[url('/assets/hero-5.png')] object-cover object-center">
+          <div className="h-full w-full flex justify-center gap-6 items-center flex-col">
+            <h1 className="text-white font-bold text-xl">New Launch</h1>
+            <p className="text-white">With new vision</p>
+            <img src="/assets/hero-4.png" loading="lazy" alt="Photo by Magicle" className="inset-0 h-40 w-30" />
+            <h1 className="text-white font-bold text-xl">Tablets</h1>
+            <p className="text-white">iPad Pro 11</p>
+          </div>
+          <div className="pointer-events-none absolute inset-0 bg-gradient-to-t from-gray-800 via-transparent to-transparent opacity-50 w-full h-full"></div>
         </div>
 
         <div className="group relative flex h-48 items-end overflow-hidden rounded-lg bg-gray-100 shadow-lg md:h-80">

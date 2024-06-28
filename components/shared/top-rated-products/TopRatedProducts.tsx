4305fd09edd9f06dc394899b41a3b51c8aa0a93d
mhossain2 <moinulhossainmahim@gmail.com>
2024-06-28 11:20:24 +0600

    refactor(6valley): added renamed files

---
+++ components/shared/top-rated-products/TopRatedProducts.tsx
@@ -0,0 +1,31 @@
+'use client';
+
+import { useQuery } from "@tanstack/react-query";
+
+import { getProducts } from "@/app/services/getProducts";
+import { IProduct } from "@/app/types/Product";
+import ProductsLoader from "../loader/ProductsLoader";
+import Product from "../product";
+
+const FeaturedProducts = () => {
+  const { data, isLoading } = useQuery({
+    queryFn: async() => await getProducts('https://6valley.6amtech.com/api/v1/products/top-rated?guest_id=1&limit=10&&offset=1'),
+    queryKey: ['top-rated']
+  })
+
+  if (isLoading) {
+    return (
+      <ProductsLoader />
+    )
+  }
+
+  return (
+    <div className="grid grid-cols-1 gap-8 sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5">
+      {data.products?.map((product: IProduct) => (
+        <Product key={product.id} product={product} />
+      ))}
+    </div>
+  )
+}
+
+export default FeaturedProducts;

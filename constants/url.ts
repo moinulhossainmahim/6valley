8c6b1ae7ab08f9d359e5a3e24116977300f1ee69
mhossain2 <moinulhossainmahim@gmail.com>
2024-06-29 11:49:42 +0600

    fix(6valley): fixed categories url

--- constants/url.ts
+++ constants/url.ts
@@ -1,5 +1,5 @@
 export const ALL_PRODUCTS_URL = 'https://6valley.6amtech.com/api/v1/products/featured?guest_id=1&limit=20&&offset=1';
-export const CATEGORIES_URL = 'https://6valley.6amtech.com/api/v1/products/categories?guest_id=1&limit=10&&offset=1';
+export const CATEGORIES_URL = 'https://6valley.6amtech.com/api/v1/categories?guest_id=1';
 export const FEATURED_PRODUCTS_URL = 'https://6valley.6amtech.com/api/v1/products/featured?guest_id=1&limit=20&&offset=1';
 export const TOP_RATED_PRODUCTS_URL = 'https://6valley.6amtech.com/api/v1/products/top-rated?guest_id=1&limit=10&&offset=1'
 export const LATEST_PRODUCTS_URL = 'https://6valley.6amtech.com/api/v1/products/latest?guest_id=1&limit=10&&offset=1';
